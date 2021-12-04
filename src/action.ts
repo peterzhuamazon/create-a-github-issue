9cfa3c800a703dc1e1ea9619c29013455897d81a
PF4Public <PF4Public@users.noreply.github.com>
2021-12-04 22:56:10 +0300

    Missing space
    I'm assuming, that `searchExistingType` is always defined.
    
    Fixes #1

--- src/action.ts
+++ src/action.ts
@@ -66,7 +66,7 @@ export async function createAnIssue (tools: Toolkit) {
   tools.log.debug('Templates compiled', templated)
 
   if (updateExisting !== null) {
-    tools.log.info(`Fetching ${searchExistingType}issues with title "${templated.title}"`)
+    tools.log.info(`Fetching ${searchExistingType} issues with title "${templated.title}"`)
     const searchExistingQuery = (searchExistingType === 'all') ? '' : `is:${searchExistingType} `
     const existingIssues = await tools.github.search.issuesAndPullRequests({
       q: `${searchExistingQuery}is:issue repo:${process.env.GITHUB_REPOSITORY} in:title ${templated.title}`
